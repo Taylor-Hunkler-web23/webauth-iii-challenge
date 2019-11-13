563bb08f8cd509d74225c104b75414813c194c1c
Taylor <52665999+taylorhh5@users.noreply.github.com>
2019-11-13 16:11:32 -0500

    Added auth router with register and login

---
+++ auth/auth-router.js
@@ -0,0 +1,67 @@
+const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken'); //1 npm i jsonwebtoken
+
+const Users = require('../users/users-model.js');
+
+
+router.post('/register', (req, res) => {
+  let user = req.body;
+
+ 
+
+  const hash = bcrypt.hashSync(user.password, 10); 
+  user.password = hash;
+  
+
+  Users.add(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+  
+});
+
+router.post('/login', (req, res) => {
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        //2 produce token 
+        const token = getJwtToken(user.username); 
+
+        //3 send the token to the client
+        res.status(200).json({
+          message: `Welcome ${user.username}!`, 
+          token // token
+        });
+      } else {
+        res.status(401).json({ message: 'Invalid Credentials' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+//4 could be in a different folder
+function getJwtToken(username) {
+  const payload = {
+    username,
+    role: 'student'
+  }
+
+  const secret = process.env.JWT_SECRET || 'safe';
+
+  const options = {
+    expiresIn: '1d'
+  };
+
+  return jwt.sign(payload, secret, options)
+}
+
+module.exports = router;
